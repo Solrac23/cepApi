fe791889731ad4c82afda050022f3d7e1a197bf0
Carlos Eduardo <eduardosantoos573@gmail.com>
2021-05-31 20:24:53 -0400

    organizando o codigo

--- src/index.js
+++ src/index.js
@@ -1,11 +1,26 @@
 const express = require('express')
 const app = express()
-const myCep = require('./services/api')
+const routes = require('../routes')
+const path = require('path')
 
-app.get('/', (req, res) => {
-  
-  res.send()
-})
+//definir o template engine
+app.set('engine views', 'ejs')
+
+// definindo caminho para a pasta
+app.set('views', path.join(__dirname, 'views'))
+ 
+// habilitar arquivos statics
+app.use(express.static(path.join(__dirname, 'public')))
+
+// aceita json 
+app.use(express.json())
+
+//usar req.body
+app.use(express.urlencoded({ extended: true }))
+
+// rotas
+app.use(routes)
+
 
 app.listen(3000, () => {
   console.log('Server is running!')
